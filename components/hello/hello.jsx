05b0ddd399242b0e11bb0de5e64be1d1be3f411f
loudong@zhuojianchina.com <loudong@zhuojianchina.com>
2017-01-05 16:51:34 +0800

    demo

---
+++ components/hello/hello.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+class Hello extends React.Component{
+    render(){
+        return <h1>hello</h1>
+    }
+}
+ReactDom.render(<Hello/>,document.getElementById('hello'));
---
+++ components/main.js
@@ -0,0 +1,6 @@
+
+import Hello from './hello/hello.jsx';
+import World from './world/world.jsx';
+import  './world/world.scss';
+import  './hello/hello.scss';
+
